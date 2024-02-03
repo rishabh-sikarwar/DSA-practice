17acf3ba5275458803cf559c05cfeeab23c3c8c2
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2024-02-03 23:04:56 +0530

    moving my local repo to online

---
+++ pattern_1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    // char a='A';
+    // int count=0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         char patt=a+count;
+    //         cout<<patt<<" ";
+    //         count++;
+    //     }
+    //     cout<<endl;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int k = 1; k < i; k++)
+    //     {
+    //         cout << " ";
+    //     }
+
+    //     for (int j = i ; j <= n; j++)
+    //     {
+    //         cout << j ;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int fir = 0; fir < n-i ; fir++)
+    {
+    cout<<fir+1;
+    }
+    for(int sec=0;sec<i;sec++){
+        cout<<"*";
+    }
+    for(int sec=0;sec<i;sec++){
+        cout<<"*";
+    }
+
+   
+    for (int fir = 0; fir < n-i ; fir++)
+    {
+    cout<<n-fir-i;
+    }
+    cout<<endl;
+}
+
+
+
+
+
+
+
+
+}
---
+++ practice_1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+// int a = 9;
+// if (a == 9)
+// {
+//     cout << "niney";
+// }
+// if (a > 0)
+// {
+//     cout << "positive";
+// }
+// else
+// {
+//     cout << "negative";
+// }
+
+// int a = 2;
+// int b = a + 1;
+// if ((a = 3) == b)
+// {
+//     cout << a;
+// }
+// else
+// {
+//     cout << a + 5;
+// }
+
+// int a=24;
+// if(a>20){
+//     cout<<"love";
+// }
+// else if(a==24){
+//     cout<<"lovely";
+// }
+// else{
+//     cout<<"babbar";
+// }
+// cout<<a;
+
+    // char a;
+    // cin >> a;
+    // if (a >= 97 && a <= 122)
+    // {
+    //     cout << "this is lowercase ";
+    // }
+    // else if (a >= 48 && a <= 57)
+    // {
+    //     cout << "this is integer number";
+    // }
+    // else if (a >= 65 && a <= 90)
+    // {
+    //     cout << "this is uppercase ";
+    // }
+    // else
+    // {
+    //     cout << "this is nothing or special character";
+    // }
+    // cout << endl;
+    // int b = a;
+    // cout << b;
+
+{
+
+
+
+    
+}
